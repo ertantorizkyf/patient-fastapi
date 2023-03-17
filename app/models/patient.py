a9ad0a96e777f1df7c301e7a599ce2864c55f40e
Rizky Ertanto <ertantorizkyf@gmail.com>
2023-03-17 21:27:17 +0700

    Update patient model and schema

--- app/models/patient.py
+++ app/models/patient.py
@@ -1,8 +1,21 @@
 from app.database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Text, Date, TIMESTAMP, text
+
 
 class Patient(Base):
-    __tablename__ = 'patient'
+    __tablename__ = 'patients'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    address = Column(String, nullable=False)
+    sex = Column(String(1), nullable=False)
+    address = Column(Text, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    pob = Column(String, nullable=False)
+    dob = Column(Date, nullable=False)
+    emergency_contact_name: Column(String, nullable=False)
+    emergency_contact_phone: Column(String, nullable=False)
+    emergenct_contact_relationship: Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False,
+                        server_default=text("CURRENT_TIMESTAMP"))
+    updated_at = Column(TIMESTAMP, nullable=True,
+                        onupdate=text("CURRENT_TIMESTAMP"))
--- app/schemas/patient.py
+++ app/schemas/patient.py
@@ -2,10 +2,22 @@ from pydantic import BaseModel
 
 
 class Patient(BaseModel):
+    id: int | None = None
     name: str
+    sex: str
     address: str
+    phone: str
+    email: str
+    pob: str
+    dob: str
+    emergency_contact_name: str
+    emergency_contact_phone: str
+    emergenct_contact_relationship: str
+    created_at: str | None = None
+    updated_at: str | None = None
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
+        
