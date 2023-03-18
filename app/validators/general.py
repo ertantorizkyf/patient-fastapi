6d712bbad161f4eb1e71c8d58e7b2e03f101729a
Rizky Ertanto <ertantorizkyf@gmail.com>
2023-03-18 12:28:27 +0700

    Add time and day name validation

--- app/validators/general.py
+++ app/validators/general.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import time
 
 
 def is_sex_valid(sex):
@@ -14,9 +15,31 @@ def is_email_valid(email):
     mail_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
     return True if re.fullmatch(mail_regex, email) else False
 
+
 def is_iso_format_date_valid(date):
     try:
         datetime.date.fromisoformat(date)
+        return True
+    except ValueError:
+        return False
+
+
+def is_day_name_valid(day):
+    days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    return True if day in days else False
+
+
+def is_time_valid(time_str):
+    try:
+        time.strptime(time_str, '%H:%M')
         return True
     except ValueError:
         return False
